788330afd72ffffba14748ad20e661016740f9bd
StViga <StViga@users.noreply.github.com>
2019-05-25 18:04:23 +0300

    remove scene

---
+++ Assets/Stviga/Assets/Character.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    [SerializeField]
+    private int lives = 5;
+
+   
+
+    [SerializeField]
+    private float speed = 4.0F;
+    [SerializeField]
+    public float jumppower = 12F;
+
+    private bool isGrounded = false;
+
+
+    private CharState State
+    {
+        get { return (CharState)animator.GetInteger("State"); }
+        set { animator.SetInteger("State", (int)value); }
+    }
+
+    new private Rigidbody2D rigidbody;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        State = CharState.Idle;
+        if (Input.GetButton("Horizontal")) Run();
+        if (Input.GetButtonDown("Jump")) Jump();
+    }
+
+    private void Run()
+    {
+        Vector3 direction = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position+direction, speed*Time.deltaTime);
+        sprite.flipX = direction.x < 0.0f;
+        State = CharState.Run;
+    }
+
+    private void Jump()
+    {
+        rigidbody.AddForce(transform.up * jumppower,ForceMode2D.Impulse);
+        State = CharState.Jump;
+
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 1.5F);
+        isGrounded = colliders.Length > 2; 
+    }
+
+
+
+}
+
+public enum CharState
+{
+    Idle,
+    Run,
+    Jump
+}
---
+++ Assets/Stviga/Assets/move.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    public float speedmove; //speed
+    public float jumppower; //jumppower
+
+    private Vector3 movevector; //
+    private float gravity = 20f;
+
+    private CharacterController ch_controller;
+    private Animator ch_animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        ch_controller = GetComponent<CharacterController>();
+        ch_animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterMove();
+        GamingGravity();
+    }
+
+    private void CharacterMove()
+    {
+        movevector = Vector2.zero;
+        movevector.x = Input.GetAxis("Horizontal") * speedmove;
+        movevector.z = Input.GetAxis("Vertical") * speedmove;
+
+        if (movevector.x != 0 || movevector.z != 0) ch_animator.SetBool("player-idle-1", true);
+        else ch_animator.SetBool("player-idle-1", false);
+
+      
+
+        movevector.y = gravity;
+        ch_controller.Move(movevector * Time.deltaTime);
+
+    }
+    private void GamingGravity()
+    {
+        if (!ch_controller.isGrounded) gravity -= 20f * Time.deltaTime;
+        else gravity = -1f;
+        if (Input.GetKeyDown(KeyCode.Space) && ch_controller.isGrounded) gravity = jumppower;
+    }
+}
+
