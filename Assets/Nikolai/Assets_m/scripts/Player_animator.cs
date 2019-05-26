2c374344489c4aad3b3dcdf96d1b1b6df9af0487
MykolaLutsyk <51016270+MykolaLutsyk@users.noreply.github.com>
2019-05-26 11:25:55 +0300

    sokoban commit

---
+++ Assets/Nikolai/Assets_m/scripts/Player_animator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_animator : MonoBehaviour
+{
+    SpriteRenderer sp;
+    public bool _anim;
+    public GameObject Player_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sp = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            sp.flipX = true;
+            Player_.GetComponent<Animator>().SetBool("anim_", true);
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            sp.flipX = false;
+            Player_.GetComponent<Animator>().SetBool("anim_", true);
+
+        }
+        else
+        {
+            Player_.GetComponent<Animator>().SetBool("anim_", false);
+        }
+
+    }
+
+}
