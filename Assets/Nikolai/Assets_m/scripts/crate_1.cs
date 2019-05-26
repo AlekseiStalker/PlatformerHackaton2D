d6fa66d6cef043b65bfcceb67f721a71b6e25e75
MykolaLutsyk <51016270+MykolaLutsyk@users.noreply.github.com>
2019-05-26 09:05:19 +0300

    puzzle commit

---
+++ Assets/Nikolai/Assets_m/scripts/crate_1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crate_1 : MonoBehaviour
+{
+    public GameObject Cherry;
+    public static int crates;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Box"))
+        {
+            crates++;
+
+            if (crates == 4)
+                Cherry.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Box"))
+            crates--;
+    }
+}
